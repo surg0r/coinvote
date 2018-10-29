6d456acb140f92576626add3fdb18e082003be24
Pete Waterland <surg0r@users.noreply.github.com>
2018-10-29 14:55:56 -0700

    catch an error with decoding binary message_hash
    cleaner way to identify message_tx via type (thanks KKS)

--- coinvote.py
+++ coinvote.py
@@ -19,11 +19,12 @@ for i in ID:
 
 # set starting blockheight to track the chain from with variable blocks
 
-blocks = 180450
+#blocks = 180450
+blocks = 175000
 
 # set safety lag in case of minor forks to chain tip
 
-block_lag = 5
+block_lag = 3
 
 
 # connect to the QRL network via the api-wrapper, announce the ID's being watched..
@@ -35,31 +36,34 @@ w = q.QRL()		# instantiate the grpc connection via the wrapper
 
 blockheight = w.node_status().node_info.block_height
 
-# continuously track ID's whilst following the current blockheight, when approaches current blockheight the script sleeps for some minutes then resumes the chase
+# continuously track ID's whilst following the current blockheight, when approaches current blockheight the script sleeps for ten minutes then resumes the chase
 
 while blocks < blockheight:
 	z = w.get_blockbynumber(blocks)
 	print ('block:', blocks, len(z.block.transactions), 'transactions')
 	for t in z.block.transactions:
-		if t.message.message_hash != b'':
-			if t.message.message_hash.decode() in ID:
-				addr = 'Q'+q.bin2hstr(w.get_addressfrompk(t.public_key).address)
+		if t.WhichOneof('transactionType') == 'message':
+			try:
+				if t.message.message_hash.decode() in ID:
+					addr = 'Q'+q.bin2hstr(w.get_addressfrompk(t.public_key).address)
 
-				if len(votes[ID.index(t.message.message_hash.decode())].addresses_balances) != 0:
-					if [item for item in votes[ID.index(t.message.message_hash.decode())].addresses_balances[0]] != addr:
+					if len(votes[ID.index(t.message.message_hash.decode())].addresses_balances) != 0:
+						if [item for item in votes[ID.index(t.message.message_hash.decode())].addresses_balances[0]] != addr:
+							votes[ID.index(t.message.message_hash.decode())].addresses_balances.append((addr, w.get_balance(addr)))
+					else:
 						votes[ID.index(t.message.message_hash.decode())].addresses_balances.append((addr, w.get_balance(addr)))
-				else:
-					votes[ID.index(t.message.message_hash.decode())].addresses_balances.append((addr, w.get_balance(addr)))
 
-				votes[ID.index(t.message.message_hash.decode())].blocks.append(blocks)
-				votes[ID.index(t.message.message_hash.decode())].txhashes.append(t.transaction_hash)
-				votes[ID.index(t.message.message_hash.decode())].calculate_vote_weight(w.node_status().coins_emitted)
+					votes[ID.index(t.message.message_hash.decode())].blocks.append(blocks)
+					votes[ID.index(t.message.message_hash.decode())].txhashes.append(t.transaction_hash)
+					votes[ID.index(t.message.message_hash.decode())].calculate_vote_weight(w.node_status().coins_emitted)
 
-				print('ID: ', t.message.message_hash.decode(), 'VOTEWEIGHT: ', votes[ID.index(t.message.message_hash.decode())].vw)
+					print('ID: ', t.message.message_hash.decode(), 'VOTEWEIGHT: ', votes[ID.index(t.message.message_hash.decode())].vw)
 
-				f = open('votes.db', 'wb')
-				pickle.dump(votes, f)
-				f.close()
+					f = open('votes.db', 'wb')
+					pickle.dump(votes, f)
+					f.close()
+			except:
+				pass
 
 	blockheight = w.node_status().node_info.block_height
 	if blocks >= blockheight - block_lag:
